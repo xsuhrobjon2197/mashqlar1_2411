7550b423440e8146422a5bc9e5ecb2f1a3cebf89
xsuhrobjon2197 <xsuhrobjon2197@gmail.com>
2025-11-24 02:23:52 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,110 @@
+#1-misol
+text = " salom dunyo "
+print(f"text: {text}")
+
+soni = 0
+for i in range(len(text)):
+    if text[i] == " ":
+        soni += 1
+
+print(f"probellar soni: {soni}")
+
+#2-misol
+matn = "o'zbekiston   resbuplikasi"
+print(f"matn: {matn}")
+
+raqam = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        raqam += 1
+
+print(f"Probellar soni: {raqam}")
+
+#3-misol
+raqamlar = "123  456  789"
+print(f"raqamlar: {raqamlar}")
+
+soni = 0
+for i in range(len(raqamlar)):
+    if raqamlar[i] == " ":
+        soni += 1
+
+print(f"Probellar soni: {soni}")
+
+#4-misol
+
+text = ""
+print(f"text: {text}")
+
+soni = 0
+for i in range(len(text)):
+    if text[i] == " ":
+     soni += 1
+
+print(f"probellar soni : {soni}")
+
+#5-misol
+matn = "      "
+print(f"matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        soni += 1
+
+print(f"Probellar soni : {soni}")
+
+#6-misol
+matn = "salom dunyo  Alik"
+print(f"matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        soni += 1
+
+print(f"Probellar soni: {soni}")
+
+#7-misol
+matn = "Toshkentshahri"
+print(f"matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        soni += 1
+
+print(f"Probellar soni: {soni}")
+
+#8-misol
+matn = " python dasturlash turi "
+print(f"matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        soni += 1
+
+print(f"Probellar soni: {soni}")
+
+#9-misol
+matn = "salom dunyo!va alik!rahmat"
+print(f"matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        soni += 1
+
+print(f"Probellar soni: {soni}")
+
+#10-misol
+matn = " a b c d e f g "
+print(f"matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        soni += 1
+
+print(f"Probellar soni: {soni}")
